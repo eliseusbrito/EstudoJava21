ea6cf6a8b86b04843384608190c573295610d7d6
eliseusbrito <eliseusbrito@gmail.com>
2024-01-14 15:14:58 -0300

    Criado projeto

---
+++ src/main/java/com/eliseu/EstudoJava21/EstudoJava21Application.java
@@ -0,0 +1,13 @@
+package com.eliseu.EstudoJava21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoJava21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoJava21Application.class, args);
+	}
+
+}
